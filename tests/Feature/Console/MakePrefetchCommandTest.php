f558bf0f1f13d6df2e7bd8e34ae8696648716644
Mior Muhammad Zaki <crynobone@gmail.com>
2020-02-18 23:40:34 +0800

    Add console test.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Feature/Console/MakePrefetchCommandTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Laravie\Prefetch\Tests\Feature\Console;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Orchestra\Canvas\Core\Testing\TestCase;
+
+class MakePrefetchCommandTest extends TestCase
+{
+    protected $files = [
+        'app/Http/Prefetch/Ping.php',
+    ];
+
+    /** @test */
+    public function it_can_generate_request_file()
+    {
+        $this->artisan('make:prefetch', ['name' => 'Ping'])
+            ->assertExitCode(0);
+
+        $this->assertFileContains([
+            'namespace App\Http\Prefetch;',
+            'use Illuminate\Http\Request;',
+            'use Illuminate\Support\Collection;',
+            'use Katsana\Prefetch\Handler;',
+            'class Ping extends Handler',
+            'public function collection(Request $request)',
+            'public function transform($data)',
+        ], 'app/Http/Prefetch/Ping.php');
+    }
+
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return ['Katsana\Prefetch\PrefetchServiceProvider'];
+    }
+}
